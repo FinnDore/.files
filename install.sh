fc7936c27a5c166dc9f655d6724e3814f77cc524
Finn <34718806+FinnDore@users.noreply.github.com>
2022-09-22 21:53:00 +0100

    added gnupg dopplerhq/cli/doppler to the install line

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # Setup
 echo installing git, youtube-dl, stripe/stripe-cli/stripe gnupg dopplerhq/cli/doppler
-brew install git youtube-dl stripe/stripe-cli/stripe
+brew install git youtube-dl stripe/stripe-cli/stripe gnupg dopplerhq/cli/doppler
 git clone https://github.com/FinnDore/.files/
 cd .files
 
