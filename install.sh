d942f22df8afafae178fc0354f8cccef3773df10
Finn <34718806+FinnDore@users.noreply.github.com>
2022-12-09 22:06:29 +0000

    btop

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ cp ./.bashrc ~/.bashrc
 # Setup
 echo installing packages and apps
 brew install git youtube-dl stripe/stripe-cli/stripe gnupg dopplerhq/cli/doppler
-brew install --cask visual-studio-code discord warp microsoft-edge github figma nordpass raycast
+brew install --cask visual-studio-code discord warp microsoft-edge github figma nordpass raycast btop
 
 git clone https://github.com/FinnDore/.files/
 cd .files
