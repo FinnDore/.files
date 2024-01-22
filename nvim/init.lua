d383e54e0b6f15f330e93b653bfa08e80c044262
Finn <34718806+FinnDore@users.noreply.github.com>
2024-01-22 20:38:46 +0000

    .

--- nvim/init.lua
+++ nvim/init.lua
@@ -132,15 +132,6 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
-  },
-
   'MunifTanjim/nui.nvim',
 
   {
